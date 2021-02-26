87e176a72549ec6b248870e264941590e4d55ab5
Timo Hanhirova <timo.hanhirova@valamis.com>
2021-02-26 22:31:27 +0200

    chore: fix navi bar underlines

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,15 +5,15 @@
 <nav>
   <ul class="flex justify-end">
     <li class="mr-6">
-      <a aria-current={path === "/about" ? "page" : undefined} href="/about"
+      <a aria-current={path === "/about/" ? "page" : undefined} href="/about/"
         >about</a
       >
     </li>
     <li class="mr-6">
       <a
         rel="prefetch"
-        aria-current={path === "/blog" ? "page" : undefined}
-        href="/blog">blog</a
+        aria-current={path === "/blog/" ? "page" : undefined}
+        href="/blog/">blog</a
       >
     </li>
   </ul>
